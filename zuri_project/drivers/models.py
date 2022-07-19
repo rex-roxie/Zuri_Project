d3fad8535647dc5c31dc794bffe6b26425f481a1
man G <rex.roxie@outlook.com>
2022-07-19 16:37:35 -0500

    add truck list and alter drivers adding phone field

--- zuri_project/drivers/models.py
+++ zuri_project/drivers/models.py
@@ -1,6 +1,5 @@
 from datetime import date
 import numbers
-from string import capwords
 from django.db import models
  # Hazard, tanker_endorsement
 
@@ -17,9 +16,10 @@ class Driver(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=50, null=True)
     medical_card_number = models.CharField(max_length=50)
     medical_card_expiry_date = models.DateField(auto_now=False, auto_now_add=False, blank=True)
-    truck_number = models.IntegerField()
-    driver_license_numbers = models.IntegerField()
+    truck_number = models.CharField(max_length=50)
+    driver_license_numbers = models.CharField(max_length=50)
     driver_license_expiry_date = models.DateField(auto_now=False, auto_now_add=False, blank=True)
     certification = models.CharField(max_length=50, choices=certification_options)
