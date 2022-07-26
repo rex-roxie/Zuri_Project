e5c3e21e4a115d1b467448e011b23a88b1f7e73e
man G <rex.roxie@outlook.com>
2022-07-26 11:00:55 -0500

    add field to driver and reset everything

--- zuri_project/drivers/forms.py
+++ zuri_project/drivers/forms.py
@@ -10,7 +10,7 @@ class SearchDriverForm(forms.Form):
 class AddDriverForm(forms.ModelForm):
     class Meta:
         model = Driver
-        fields = ['first_name', 'last_name', 'email', 'phone', 'medical_card_number', 'medical_card_expiry_date', 'truck_number', 'driver_license_numbers', 'driver_license_expiry_date', 'certification']
+        fields = ['first_name', 'last_name', 'email', 'phone', 'medical_card_number', 'medical_card_expiry_date', 'truck_number', 'driver_license_numbers', 'driver_license_expiry_date', 'certification', 'citizenship']
         
 class DeleteDriverForm(forms.ModelForm):
     class Meta:
--- zuri_project/drivers/models.py
+++ zuri_project/drivers/models.py
@@ -10,19 +10,28 @@ certification_options = (
     ('neither', 'Neither')
 )
 
+# PR stands for permanent resident
+
+citizenship_options = (
+    ('citizen', 'C (citizen)'),
+    ('non_citizen', 'PR (permanent residence)'),
+    ('work_permit', 'WP (work permit)')
+)
+
 
 # Create your models here.
 class Driver(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
-    phone = models.CharField(max_length=50, null=True)
+    phone = models.CharField(max_length=50, default=None)
     medical_card_number = models.CharField(max_length=50)
     medical_card_expiry_date = models.DateField(auto_now=False, auto_now_add=False, blank=True)
     truck_number = models.CharField(max_length=50)
     driver_license_numbers = models.CharField(max_length=50)
     driver_license_expiry_date = models.DateField(auto_now=False, auto_now_add=False, blank=True)
     certification = models.CharField(max_length=50, choices=certification_options)
+    citizenship = models.CharField(max_length=50, choices=citizenship_options)
 
     def __str__(self):
         return self.first_name
