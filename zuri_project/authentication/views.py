4c13f5435318e043ef702b8b9f4ad2a3cef49df5
man G <rex.roxie@outlook.com>
2022-07-26 11:46:27 -0500

    finish all functionality

--- zuri_project/authentication/views.py
+++ zuri_project/authentication/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def index(request):
     return render(request, 'authentication/index.html', {})
+
+@login_required
+def dashboard(request):
+    return render(request, 'authentication/dashboard.html', {})
--- zuri_project/zuri_project/urls.py
+++ zuri_project/zuri_project/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from authentication.views import index
+from authentication.views import index, dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('dashboard', dashboard, name="dashboard"),
     path('', include('django.contrib.auth.urls')),
     path('drivers/', include('drivers.urls')),
     path('loads/', include('loads.urls')),
